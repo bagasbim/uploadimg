38bbd254014121edf8b12e68690cde68aa2d4e24
bagasbim <133872707+bagasbim@users.noreply.github.com>
2023-05-17 22:25:40 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+$host       = "localhost";
+$user       = "root";
+$password   = "P@ssw0rd";
+$database   = "db_upload";
+$koneksi    = mysqli_connect($host, $user, $password, $database);
+?>
